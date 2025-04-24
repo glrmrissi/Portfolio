4055790381b9ab41cf8f7ae2d37da0105bef17b5
glrmrissi <guilhermerissi04112006@gmail.com>
2025-04-23 23:52:40 -0300

    slider added, but for while don't have nothing

---
+++ src/components/Projects/Projects.tsx
@@ -0,0 +1,13 @@
+import { EmblaCarousel } from "./Slider/Slider";
+
+const Projects = () => {
+    return (
+        <>
+            <section className="projects-section">
+                <EmblaCarousel></EmblaCarousel>
+            </section>
+        </>
+    )
+}
+
+export default Projects;
