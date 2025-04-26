04b28f7ae087e442d6040fd458f77fe96c7d7913
glrmrissi <guilhermerissi04112006@gmail.com>
2025-04-26 20:20:22 -0300

    slider overlay completed

--- src/components/Projects/Slider/ContentSlider/ContentSlider.tsx
+++ src/components/Projects/Slider/ContentSlider/ContentSlider.tsx
@@ -6,21 +6,37 @@ const ContentSlider = () => {
     <>
       <div className="embla__slide">
         <div className="embla__slide__number">
-          <h1 className="embla__overlay__title">MangaKitsu</h1>
+          <div className="embla__overlay__directs">
+            <h1 className="embla__overlay__title">MangaKitsu</h1>
+            <div>
+              <a href="https://github.com/glrmrissi/mangasKitsu" target="_blank">
+                <i className="bi bi-github theme-icon"></i>
+                Code
+              </a>
+            </div>
+            <div>
+              <a href="https://glrmrissi.github.io/mangasKitsu/" target="_blank">
+                <i className="bi bi-box-arrow-up-right"></i>
+                On web
+              </a>
+            </div>
+          </div>
           <img src={MangaKitsu} />
         </div>
       </div>
       <div className="embla__slide">
         <div className="embla__slide__number">
-        <h1 className="embla__overlay__title">Sistema de busca usando a Kitsu API</h1>
-        <img src={KitsuSearch} />
+          <h1 className="embla__overlay__title">
+            Sistema de busca usando a Kitsu API
+          </h1>
+          <img src={KitsuSearch} />
         </div>
       </div>
       <div className="embla__slide">
         <div className="embla__slide__number">
           <h1>JS</h1>
         </div>
-      </div>        
+      </div>
       <div className="embla__slide">
         <div className="embla__slide__number">
           <h1>TS</h1>
