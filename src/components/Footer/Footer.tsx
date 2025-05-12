275f4493ee24ede281ddf4f966f11780e8438d5e
glrmrissi <guilhermerissi04112006@gmail.com>
2025-05-12 13:57:35 -0300

    color more reds and added footer

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,12 @@
+const Footer = () => {
+    return (
+        <>
+            <footer className="footer-container">
+
+            </footer>
+            <div className="wave"></div>
+        </>
+    )
+}
+
+export default Footer;
